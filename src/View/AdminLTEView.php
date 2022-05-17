87883a3aefe692098f83ea6c8b7e71fd5afa8763
Csipke Ferenc <csipke.ferenc@gmail.com>
2022-05-17 09:26:27 +0200

    adminlteview default layout

---
+++ src/View/AdminLTEView.php
@@ -0,0 +1,9 @@
+<?php
+namespace AdminLTE\View;
+
+use \App\View\AppView;
+
+class AdminLTEView extends AppView {
+	
+	public $layout = 'AdminLTE.default';
+}
---
+++ templates/layout/default.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<?php echo $this->fetch('content'); ?>
+</body>
+</html>
